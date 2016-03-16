510c204a9753e528804ce0706b42dff157c001b5
Tran Quang Trung <tran.quang.trung@framgia.com>
2016-03-16 17:09:54 +0700

    finish task change status subject of course

--- db/migrate/20160310013310_create_course_subjects.rb
+++ db/migrate/20160310013310_create_course_subjects.rb
@@ -1,7 +1,7 @@
 class CreateCourseSubjects < ActiveRecord::Migration
   def change
     create_table :course_subjects do |t|
-      t.integer :status
+      t.integer :status, default: 0
       t.references :course, index: true, foreign_key: true
       t.references :subject, index: true, foreign_key: true
 
